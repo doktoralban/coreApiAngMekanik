487b6204134a97460243fe721ec51a6d0fdb9a94
Aydın Türker <doktoralban@outlook.com>
2021-05-20 20:35:04 +0300

    Add project files.

---
+++ coreApiAngMekanik/Models/dbContextAPP.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreApiAngMekanik.Models
+{
+    public class dbContextAPP:DbContext
+    {
+        public dbContextAPP(DbContextOptions<dbContextAPP> options) : base(options)
+        {
+
+        }
+
+
+
+
+
+    }
+}
